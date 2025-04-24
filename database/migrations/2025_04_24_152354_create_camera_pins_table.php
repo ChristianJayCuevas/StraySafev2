2bef47525d93d1e3d1c01f067d7b01de2a436592
ChristianJayCuevas <christianjaycuevas@gmail.com>
2025-04-24 23:29:47 +0800

    Created Migration for Camera Pin

---
+++ database/migrations/2025_04_24_152354_create_camera_pins_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('camera_pins', function (Blueprint $table) {
+            $table->id();
+            $table->string('camera_name')->nullable();
+            $table->string('hls_url')->nullable();
+            $table->text('camera_description')->nullable();
+            $table->decimal('latitude', 10, 7);
+            $table->decimal('longitude', 10, 7);
+            $table->foreignId('user_map_id')->nullable()->constrained('user_maps')->nullOnDelete();
+            $table->index('user_map_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('camera_pins');
+    }
+};
